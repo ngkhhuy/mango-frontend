71fec92238f4e2feaa915fbcc0bb422a5e76c915
ngkhhuy <khanhhuychc@gmail,com>
2025-04-06 18:02:43 +0700

    huy1

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -23,7 +23,7 @@ export default function LoginScreen() {
     try {
       console.log("Đang gửi yêu cầu đăng nhập với username:", username)
       
-      const response = await fetch("https://mango-backend-fnhz.onrender.com/api/auth/login", {
+      const response = await fetch("https://mango-backend-ub33.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
